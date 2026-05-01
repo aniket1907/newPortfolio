899d498a91c197041492945578e62adfc3ab24a5
aniket1907 <aniketchoudhari2001@gmail.com>
2026-05-02 00:24:31 +0530

    Title and foote changes

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="text-center py-6 border-t border-white/10 text-gray-400 text-sm">
-      © 2026 Manoj. All rights reserved.
+      © 2026 Aniket. All rights reserved.
     </footer>
   );
 };
